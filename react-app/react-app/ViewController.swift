89bf3579bdb4c16499d489b4905f77ad1d4b55f1
Yuanming Chen <yuanming@Yuanmings-Mac-minilocal.local>
2017-11-08 14:06:31 -0700

    Load google webpage

--- react-app/react-app/ViewController.swift
+++ react-app/react-app/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = URL (string: "https://www.google.com")
+        let requestObj = URLRequest(url: url!)
+        webView.loadRequest(requestObj)
     }
 
     override func didReceiveMemoryWarning() {
