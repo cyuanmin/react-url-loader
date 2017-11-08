094ac2b92e0516f2fa6e9bd2c041c0b2c2d6cc96
Yuanming Chen <yuanming@Yuanmings-Mac-minilocal.local>
2017-11-08 12:27:46 -0700

    Added base swift project

---
+++ react-app/react-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  react-app
+//
+//  Created by Yuanming Chen on 11/8/17.
+//  Copyright © 2017 Yuanming Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
