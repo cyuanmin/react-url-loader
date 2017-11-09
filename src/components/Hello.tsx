37c5eee5035f6e0ac2eae21ad9ad62bc89d132cf
Yuanming Chen <yuanming@Yuanmings-Mac-minilocal.local>
2017-11-08 20:08:12 -0700

    Convert function to class for react

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,6 +1,38 @@
 import * as React from "react";
 
-export interface HelloProps { compiler: string; framework: string; }
+/*
+export interface HelloProps { compiler: string; framework: string;}
 const sum: number = 1 + 2;
 export const Hello: (props: HelloProps) => JSX.Element = 
-(props: HelloProps): JSX.Element => <h1>Hello from {props.compiler} and {props.framework}! 1+2= {sum}</h1>;
+(props: HelloProps): JSX.Element => {
+
+    return <h1>Hello from {props.compiler} and {props.framework}! 1+2= {sum}</h1>;
+}
+*/
+
+interface IHelloProps {
+    compiler: string
+    framework: string,
+    imageUrl: string
+}
+
+interface IHelloState {
+    blob: Blob;
+    info: string;
+}
+
+export class Hello extends React.Component<IHelloProps, IHelloState>{
+    constructor(props: IHelloProps){
+        super(props);
+        this.state = {
+            blob: null,
+            info: "before initialize"
+        };
+    }
+
+    render() {
+        return (
+            <h1>Hello from {this.props.compiler} and {this.props.framework}! 1+2= {this.state.info}</h1>
+        )
+      }
+}
